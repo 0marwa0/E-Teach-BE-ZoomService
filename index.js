3cac6aa4df8e25c5c83a0cf5e95a78096932e4ce
=marwa <=marwajawad19@gmail>
2025-05-21 14:55:18 +0300

    solving cors issu

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "http://localhost:3000", // or '*' to allow all
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 const zoomRoutes = require("./routes/zoomRoutes");
-
 app.use(express.json());
 app.use("/zoom", zoomRoutes);
 app.get("/", (req, res) => {
