105b2b0853b70e0fe47805e3bea1f6b89495343b
=marwa <=marwajawad19@gmail>
2025-05-21 12:41:59 +0300

    adding test route

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const zoomRoutes = require("./routes/zoomRoutes");
 
 app.use(express.json());
 app.use("/zoom", zoomRoutes);
-
+app.get("/", (req, res) => {
+  res.send("Zoom service!!");
+});
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Zoom service running on port ${PORT}`));
